43e83e36535df292ba8e3435c4bdf6ee75bc7791
tccmobile <William.smith@tulsacc.edu>
2023-08-21 14:54:34 -0500

    Create overkill.cpp

---
+++ overkill.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using  std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+
+int main() {
+  string name;
+  cout << "Hello World!\n"<<endl;
+}
